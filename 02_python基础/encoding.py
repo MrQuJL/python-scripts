25a6f835bbab46fdd1d719882fb63a15c40c4635
MrQuJL <909301740@qq.com>
2018-05-09 15:02:18 +0800

    python 格式化，类似C语言，format来进行格式化，%% 转义百分号

--- 02_python基础/encoding.py
+++ 02_python基础/encoding.py
@@ -24,6 +24,33 @@ print(len('中文'.encode('utf8')))
 
 print(len(b'ABC'))
 
+print('''换行符
+''')
+
+print('Hello, %s' % 'world')
+
+print('Hi, %s, you have $%d' % ('Michael', 1000000))
+
+print('%2d-%02d' % (3, 1))
+
+print('%.2f' % 3.1415926)
+
+print('Age: %s. Gender: %s' % (25, True))
+
+print("growth rate: %d%%" % 7)
+
+print('Hello, {0}, 成绩提升了 {1:.1f}%'.format('小明', 17.125))
+
+s1 = 72
+
+s2 = 85
+
+r = (s2 - s1) / s1
+
+r = r * 100
+
+print('%.2f%%' % r)
+
 
 
 
