5182c90c668e7035981279f620e41a92eb5f04a9
MrQuJL <909301740@qq.com>
2018-05-11 18:50:18 +0800

    python 调用函数，abs，int，给函数起别名，数据类型转换,etc

---
+++ 03_函数/call.py
@@ -0,0 +1,47 @@
+print(abs(100))
+
+print(abs(-20))
+
+print(abs(12.34))
+
+print(help(abs))
+
+#print(abs(1, 2))
+
+print(max(1, 3, 99, 8))
+
+print(int('123'))
+
+print(int(12.34))
+
+print(float('12.34'))
+
+print(str(1.23))
+
+print(str(100))
+
+print(bool(1))
+
+print(bool(''))
+
+print('''------''')
+
+a = abs
+
+print(a(-1))
+
+print(str(hex(255)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
