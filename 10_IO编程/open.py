9162615868c13826c9e090c4d55bb7b07d77f9cb
MrQuJL <909301740@qq.com>
2018-05-23 19:12:08 +0800

    pythonIO编程：读文件，写文件，读图片，写图片，追加文件

---
+++ 10_IO编程/open.py
@@ -0,0 +1,42 @@
+#f = open('D:\\file !important\\study-python\\07_面向对象编程\\perms.py', 'r')
+
+#print(f.read())
+
+#f.close()
+
+#try:
+#	f = open('D:\\file !important\\study-python\\07_面向对象编程\\perms.py', 'r')
+	#print(f.read())
+#finally:
+#	if f:
+#		f.close()
+
+#with open('D:\\file !important\\study-python\\07_面向对象编程\\perms.py', 'r') as f:
+	#print(f.read())
+
+
+#f = open('D:\\file !important\\study-python\\07_面向对象编程\\perms.py', 'r')
+
+#for line in f.readlines():
+#	print(line.strip())
+
+#f.close()
+
+#a = open('D:\\picture\\1.jpg', 'rb')
+
+#print(a.read())
+#a.close()
+
+#f = open('D:\\file !important\\study-python\\10_IO编程\\README.md', 'r', encoding='utf-8', errors='ignore')
+
+#print(f.read())
+
+#f.close()
+
+f = open('D:\\file !important\\study-python\\10_IO编程\\temp.txt', 'a')
+
+f.write('Hello world\n')
+
+f.close()
+
+
