ae6bca53a9c33ce316363985d764cf5e1e2ed83b
MrQuJL <909301740@qq.com>
2018-05-29 21:17:53 +0800

    python datetime

--- 13_常用内建模块/dt.py
+++ 13_常用内建模块/dt.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime,timedelta,timezone
 
 #now = datetime.now()
 
@@ -19,16 +19,15 @@ cday = datetime.strptime('2018-5-28 9:52:10', '%Y-%m-%d %H:%M:%S')
 #print(cday)
 
 now = datetime.now()
-print(now.strftime('%a, %b %d %H:%M'))#星期几，月，日
+#print(now.strftime('%a, %b %d %H:%M'))#星期几，月，日
 
+#print(now)
 
+#print(now + timedelta(hours=10))
 
+#print(now - timedelta(days=1))
 
-
-
-
-
-
+#print(now + timedelta(days=2, hours=12))
 
 
 
