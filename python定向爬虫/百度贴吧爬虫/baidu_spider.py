a57b77d2df0f27046525999abc4bf9d30f0c4aee
MrQuJL <909301740@qq.com>
2018-12-25 22:27:14 +0800

    start baidu spider

---
+++ python定向爬虫/百度贴吧爬虫/baidu_spider.py
@@ -0,0 +1,14 @@
+# -*- coding:utf8 -*-
+
+import requests
+from lxml import etree
+import re
+
+# 目标网站：http://tieba.baidu.com/p/3522395718
+# 目标内容：跟帖用户名，跟帖内容，跟帖时间
+# 涉及知识：requests获取网页内容，xPath提取内容，map实现多线程爬虫
+
+
+
+
+
