06e1cc6dab28d0b63670d2d24ff3c8ab5a2d117a
MrQuJL <909301740@qq.com>
2018-12-26 23:28:05 +0800

    use re fixed xpath's bug

--- python定向爬虫/百度贴吧爬虫/baidu_spider.py
+++ python定向爬虫/百度贴吧爬虫/baidu_spider.py
@@ -30,22 +30,32 @@ def get_code(url):
 
 
 def spider(html):
+    objs = []
     selector = etree.HTML(html)
     list = selector.xpath('//div[starts-with(@class, "l_post j_l_post l_post_bright")]')
     for each in list:
         # 2.1 跟帖用户名
         obj = json.loads(each.xpath('@data-field')[0])
-        print(obj['author']['user_name'])
+        name = obj['author']['user_name']
+        # print(name)
         # 2.2 跟帖内容
         # msg = each.xpath('div[@class="d_post_content_main"]/cc/div[starts-with(@class, "d_post_content j_d_post_content")]/text()')
-        msg = each.xpath('div[@class="d_post_content_main"]/cc')
+        # msg = each.xpath('div[@class="d_post_content_main"]/cc')
         # print(msg)
         # 2.3 跟帖时间
-        print(obj['content']['date'] + '\n')
-    ct = selector.xpath('//div[starts-with(@class, "l_post j_l_post l_post_bright")]/div[@class="d_post_content_main"]/cc/div[@class="d_post_content j_d_post_content  clearfix"]/text()')
-    for each in ct:
-        print(each)
-
+        time = obj['content']['date']
+        # print(obj['content']['date'])
+        user = UserInfo(name, '', time)
+        objs.append(user)
+    # bug: xpath爬不到回复的内容，用正则替代
+    content = re.findall('class="d_post_content j_d_post_content  clearfix" style="display:;">(.*?)<', html, re.S)
+    i = 0
+    for each in content:
+        cot = each.replace(' ', '').replace('\n', '')
+        objs[i].content = cot
+        # print(cot)
+        i += 1
+    return objs
 
 # 3.输出到文件
 
@@ -61,6 +71,10 @@ page = []
 # 生成url page
 
 html = get_code("http://tieba.baidu.com/p/3522395718?pn=1")
-spider(html)
+objs = spider(html)
 
-
+for i in objs:
+    print(i.name)
+    print(i.content)
+    print(i.time)
+    print('---')
